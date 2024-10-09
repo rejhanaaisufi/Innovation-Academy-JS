8037419c969aa6aab13a8dd0c37641828f04f1d6
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-10-09 19:34:31 +0200

    Sum, substract, divide, multiply of numbers

---
+++ Lesson1/main.js
@@ -0,0 +1,20 @@
+let firstName = "Rejhana";    
+let studying = true;         
+let students = 37;
+let nullOne = null;
+let study;
+
+
+
+let [numOne, numTwo, numThree, numFour, numFive] = [10, 27, 34, 18, 99];
+
+let sum = numOne + numTwo + numThree + numFour + numFive;
+let subtract = numOne - numTwo - numThree - numFour - numFive;
+let divide = numOne / numTwo / numThree / numFour / numFive;
+let multiply = numOne * numTwo * numThree * numFour * numFive;
+
+
+console.log("The sum of the numbers: " , numOne, numTwo, numThree, numFour, numFive," is:", sum);
+console.log("The subtract of the numbers: ", numOne, numTwo, numThree, numFour, numFive," is:", subtract); 
+console.log("The divide of the numbers: ", numOne, numTwo, numThree, numFour, numFive," is:", divide);
+console.log("The multiply of the numbers: ", numOne, numTwo, numThree, numFour, numFive,"is:", multiply);
