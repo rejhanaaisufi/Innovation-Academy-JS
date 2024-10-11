eaaa3c3c1dabe82ac4c18d706875d924c4f21eb0
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-10-11 16:13:15 +0200

    Update main.js

--- Lesson1/main.js
+++ Lesson1/main.js
@@ -8,13 +8,16 @@
 
 let [numOne, numTwo, numThree, numFour, numFive] = [10, 27, 34, 18, 99];
 
-let sum = numOne + numTwo + numThree + numFour + numFive;
-let subtract = numOne - numTwo - numThree - numFour - numFive;
+let sumOne = numOne + numTwo + numThree + numFour + numFive;
+let sub = numOne - numTwo - numThree - numFour - numFive;
 let divide = numOne / numTwo / numThree / numFour / numFive;
 let multiply = numOne * numTwo * numThree * numFour * numFive;
 
-
-console.log("The sum of the numbers: " , numOne, numTwo, numThree, numFour, numFive," is:", sum);
-console.log("The subtract of the numbers: ", numOne, numTwo, numThree, numFour, numFive," is:", subtract); 
+console.log("The sum of the numbers: " , numOne, numTwo, numThree, numFour, numFive," is:", sumOne);
+console.log("The subtract of the numbers: ", numOne, numTwo, numThree, numFour, numFive," is:", sub); 
 console.log("The divide of the numbers: ", numOne, numTwo, numThree, numFour, numFive," is:", divide);
 console.log("The multiply of the numbers: ", numOne, numTwo, numThree, numFour, numFive,"is:", multiply);
+
+
+
+
